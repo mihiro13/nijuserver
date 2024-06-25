91e4b5c642971373fcd5eb9e84d45270105760a1
mihiro13 <26hrosub@gmail.com>
2024-06-25 21:01:26 +0900

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,9 @@
-function toggleMenu() {
-    var menu = document.getElementById("sideMenu");
-    menu.style.width = menu.style.width === "250px" ? "0" : "250px";
-    var bars = document.querySelector(".hamburger-menu");
-    bars.classList.toggle("change");
+function changeTable() {
+    var selectBox = document.getElementById("selectBox");
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    var tables = document.getElementsByTagName("table");
+    for (var i = 0; i < tables.length; i++) {
+        tables[i].style.display = "none";
+    }
+    document.getElementById(selectedValue).style.display = "table";
 }
